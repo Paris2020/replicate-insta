e61d03a3ae4a4d082a68a31612c6970483691c78
Nelly <dineo06.moseki@gmail.com>
2026-09-07 15:04:32 +0200

    Feat: Update home.blade.php file

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,13 +3,16 @@
         Welcome
     </x-slot:title>
     <div class="max-w-2xl mx-auto">
+        @foreach ($chirps as $chirp)
             <div class="card bg-base-100 shadow mt-8">
                 <div class="card-body">
                     <div>
-                        <h1 class="text-3xl font-bold">Welcome to Chirper!</h1>
-                        <p class="mt-4 text-base-content/60">Deployed yet?!</p>
+                        <div class="font-semibold">{{ $chirp['author'] }}</div>
+                        <div class="mt-1">{{ $chirp['message'] }}</div>
+                        <div class="text-sm text-gray-500 mt-2">{{ $chirp['time'] }}</div>
                     </div>
                 </div>
             </div>
-        </div>
+        @endforeach
+    </div>
 </x-layout>
