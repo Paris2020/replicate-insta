524b642073ceb92205f53cc85a1d6eab1fb0a180
Nelly <dineo06.moseki@gmail.com>
2026-09-08 14:52:12 +0200

    Feat: add new route for login

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Auth\Logout;
+use App\Http\Controllers\Auth\Login;
 use App\Http\Controllers\Auth\Register;
 use App\Http\Controllers\ChirpController;
 use Illuminate\Support\Facades\Route;
@@ -23,4 +24,8 @@ Route::view('/register', 'auth.register')->middleware('guest')->name('register')
 Route::post('/register', Register::class)->middleware('guest');
 
 // Logout route
-Route::post('/logout', Logout::class)->middleware('auth');
+Route::post('/logout', Logout::class)->middleware('auth')->name('logout');
+
+//Login route
+Route::view('/login', 'auth.login')->middleware('guest')->name('login');
+Route::post('login', Login::class)->middleware('guest');
