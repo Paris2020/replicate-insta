acb1baa3bb9242bca73b38dd6a1fef2c2507a053
Nelly <dineo06.moseki@gmail.com>
2026-09-08 14:50:09 +0200

    Feat: when user logs out invalidate and regenrate token for the session

--- app/Http/Controllers/Auth/Logout.php
+++ app/Http/Controllers/Auth/Logout.php
@@ -13,9 +13,11 @@ class Logout extends Controller
      */
     public function __invoke(Request $request)
     {
-        $user = $request->user();
         
-        Auth::logout($user);
+        Auth::logout();
+        
+        $request->session()->invalidate();
+        $request->session()->regenrateToken();
 
         return redirect('/')->with('success','You\'ve successfully logged out!');
     }
